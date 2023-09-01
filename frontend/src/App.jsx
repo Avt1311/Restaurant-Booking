1cfe3ccc92289bc78967b1d234a0f8a0bc9f0ba6
Aviral Tripathi <aviralcsss@gmail.com>
2023-09-01 10:42:10 +0530

    m

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,13 +4,7 @@ import Home from "./components/Home";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import Navbar from "./components/Navbar";
-import EventHandling from "./components/EventHandling";
-import StateManagement from "./components/StateManagement";
-import TodoList from "./components/TodoList";
-import ProductList from "./components/ProductList";
-import ManageUser from "./components/ManageUser";
 import { Toaster } from "react-hot-toast";
-import UpdateUser from "./components/UpdateUser";
 import { AnimatePresence } from "framer-motion";
 import { UserProvider } from "./UserContext";
 import Profile from "./components/Profile";
@@ -29,13 +23,7 @@ function App() {
             <Route element={<Home />} path="home" />
             <Route element={<Signup />} path="signup" />
             <Route element={<Login />} path="login" />
-            <Route element={<EventHandling />} path="event" />
-            <Route element={<StateManagement />} path="state" />
-            <Route element={<TodoList />} path="todo" />
-            <Route element={<ProductList />} path="list" />
-            <Route element={<ManageUser />} path="manageuser" />
             <Route element={<UserAuth> <Profile /> </UserAuth>} path="profile" />
-            <Route element={<UpdateUser />} path="updateuser/:id" />
           </Routes>
         </AnimatePresence>
         </UserProvider>
